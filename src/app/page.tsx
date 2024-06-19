6476b2e84ee18c748c3ace1b881172bb0ca80647
Brahma behera <beherabrahmananda77@gmail.com>
2024-06-20 00:09:15 +0530

    added new table and updated get images code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,17 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <p>Hellow world</p>
-        <div>
-          {posts.map((post) => (
-            <h2 key={post.id}>{post.name}</h2>
+        <div className="flex w-full flex-wrap gap-12">
+          {images.map((image) => (
+            <div key={image.id} className="flex  h-[20rem] w-[20rem] flex-col gap-2">
+              <img src={image.url} alt={image.name} className="h-full w-full  " />
+              <h2>{image.name}</h2>
+            </div>
           ))}
         </div>
       </div>
